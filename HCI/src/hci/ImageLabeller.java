162bf77c8639c2d63bdd017f83704ff82281cd14
shaun <shaunmccloskey@hotmail.com>
2012-10-25 13:07:33 +0100

    can browse dir but not load the image

--- HCI/src/hci/ImageLabeller.java
+++ HCI/src/hci/ImageLabeller.java
@@ -8,10 +8,15 @@ import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTree;
 import javax.swing.ListSelectionModel;
 import javax.swing.WindowConstants;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
@@ -27,6 +32,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Vector;
 
 
 /**
@@ -58,6 +65,9 @@ public class ImageLabeller extends JFrame{
 	private static final long serialVersionUID = 1L;
 
 
+	static ImageLabeller window = new ImageLabeller();
+	
+	
 	/**
 	 * main window panel
 	 */
@@ -86,7 +96,13 @@ public class ImageLabeller extends JFrame{
 	
 	FileTree fileTree = new FileTree(new File("../HCI/src/images"));
 	
-	String currentFile ="U1003_0000";
+	String currentFile = "U1003_0000.jpg";
+	
+	String currentFileOut ="U1003_0000";
+	
+	JFrame loadFrame = new JFrame("load");
+	
+	String currentNode ="";
 	
 	/**
 	 * handles New Object button action
@@ -101,16 +117,132 @@ public class ImageLabeller extends JFrame{
 	}
 	
 	
-	public void load(){
+	
+	DefaultMutableTreeNode addNodes(DefaultMutableTreeNode currentTop, File dir) {
+	    String currentPath = dir.getPath();
+	    DefaultMutableTreeNode currentDir = new DefaultMutableTreeNode(currentPath);
+	    if (currentTop != null) { // should only be null at root
+	      currentTop.add(currentDir);
+	    }
+	    
+	    Vector ol = new Vector();
+	    String[] tmp = dir.list();
+	    
+	    for (int i = 0; i < tmp.length; i++)
+	    	ol.addElement(tmp[i]);
+	    
+	    Collections.sort(ol, String.CASE_INSENSITIVE_ORDER);
+	    File f;
+	    Vector files = new Vector();
+	    
+	    // Make two passes, one for Dirs and one for Files. This is #1.
+	    for (int i = 0; i < ol.size(); i++) {
+	      String thisObject = (String) ol.elementAt(i);
+	      String newPath;
+	      if (currentPath.equals("."))
+	        newPath = thisObject;
+	      else
+	        newPath = currentPath + File.separator + thisObject;
+	      if ((f = new File(newPath)).isDirectory())
+	        addNodes(currentDir, f);
+	      else
+	        files.addElement(thisObject);
+	    }
+	    // Pass two: for files.
+	    for (int fnum = 0; fnum < files.size(); fnum++)
+	      currentDir.add(new DefaultMutableTreeNode(files.elementAt(fnum)));
+	    return currentDir;
+	  }
+	
+	
+	
+	public void load(File dir){
 		System.out.println("tryng to load a file");
 		
-		while(fileTree.load()){
-			currentFile = fileTree.currentNode;
-		}
-		System.out.println("the currentfile is " + currentFile);
+		loadFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		loadFrame.setLayout(new BorderLayout());
+		JTree tree = new JTree(addNodes(null, dir));   
+	    
+	    
+	    // Add a listener
+	    tree.addTreeSelectionListener(new TreeSelectionListener() {
+	      public void valueChanged(TreeSelectionEvent e) {
+	        DefaultMutableTreeNode SelectedNode = (DefaultMutableTreeNode) e
+	            .getPath().getLastPathComponent();
+	        currentNode = "" + SelectedNode;
+	        System.out.println("You selected " + SelectedNode);
+	      }
+	    });
+
+	    JButton loadButton = new JButton("Load");
+	    loadButton.setMnemonic(KeyEvent.VK_N);
+		loadButton.setSize(50, 20);
+		loadButton.setEnabled(true);
+		loadButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			    	System.out.println("selected : " + currentNode);
+			    	currentFile = currentNode;
+			    	int stringIndex = currentFile.indexOf(".jpg");
+			    	currentFileOut = currentFile.substring(0, stringIndex);
+			    	
+			    	loadFrame.dispose();
+			    	
+			    	//need to call a function that will change the image, will setUpGui work?
+			    	try {
+						reLoad(currentFile);
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+			    	
+			    	
+			    	
+			}
+		});
+		loadButton.setToolTipText("Click to confirm the load");
+		
+		
+	    JButton cancelButton = new JButton("Cancel");
+	    cancelButton.setMnemonic(KeyEvent.VK_N);
+		cancelButton.setSize(50, 20);
+		cancelButton.setEnabled(true);
+		cancelButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			    	System.out.println("cancel file load");
+			}
+		});
+		cancelButton.setToolTipText("Click to cancel load");
+		
+		
+	    // Lastly, put the JTree into a JScrollPane.
+	    JScrollPane scrollpane = new JScrollPane();
+	    scrollpane.getViewport().add(tree);
+	    loadFrame.add(BorderLayout.NORTH, scrollpane);
+	    
+	    //add the butons for load and cancel to the panel
+	    loadFrame.add(BorderLayout.EAST, loadButton);
+	    loadFrame.add(BorderLayout.WEST, cancelButton);
+		
+	    
+	    loadFrame.pack();
+		loadFrame.setVisible(true);
+		
 		
 		// need to reload GUI with new fileName
 		//could have a while loop thats waits for the user to select a file when selected close FileTree and set CurrentFile to correct file
+	}
+	
+	public void reLoad(String file) throws Exception
+	{
+		
+		getPolygons(this.window);
+		//imagePanel = new ImagePanel("/afs/inf.ed.ac.uk/user/s09/s0930584/workspace/HCI/src/images/" + file, polygonsList, polygonNames);
+		//imagePanel.setOpaque(true); //content panes must be opaque
+		
+		//window.validate();
+		//window.repaint();
+		
 	}
 	
 	public void displayList(ArrayList<String> polygonNames) {
@@ -162,7 +294,7 @@ public class ImageLabeller extends JFrame{
 		  			}
 		  			//write the objects to a file
 		  			
-		  			File f = new File(currentFile + ".txt");
+		  			File f = new File(currentFileOut + ".txt");
 		  			if(!f.exists())
 		  			{
 		  				try {
@@ -170,13 +302,13 @@ public class ImageLabeller extends JFrame{
 						} catch (IOException e) {
 							e.printStackTrace();
 						}
-		  				System.out.println("New file \" " + currentFile + ".txt\" has been created to the current directory");
+		  				System.out.println("New file \" " + currentFileOut + ".txt\" has been created to the current directory");
 		  			}
 		  			
 		  			
 		  			FileWriter filey = null;
 					try {
-						filey = new FileWriter( currentFile + ".txt");
+						filey = new FileWriter( currentFileOut + ".txt");
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}
@@ -260,17 +392,30 @@ public class ImageLabeller extends JFrame{
 		loadButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				load();
+				load(new File("../HCI/src/images"));
 			}
 		});
 		newPolyButton.setToolTipText("click to load objects");
 		
+		
+		JButton checkButton = new JButton("check File");
+		checkButton.setMnemonic(KeyEvent.VK_N);
+		checkButton.setSize(50, 20);
+		checkButton.setEnabled(true);
+		checkButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e ) {
+				System.out.println("the file is set to : " + currentFile);
+				System.out.println("the Out file is: " + currentFileOut);
+			}
+		});
 		
 		
 		displayList(polygonNames);
 		
 		toolboxPanel.add(newPolyButton);
 		toolboxPanel.add(loadButton);
+		toolboxPanel.add(checkButton);
 		toolboxPanel.add(listScroller);
 
 		//add toolbox to window
@@ -306,14 +451,14 @@ public class ImageLabeller extends JFrame{
 			return;
 		}
 		
-		File f = new File(currentFile + ".txt");
+		File f = new File(currentFileOut + ".txt");
 		if(!f.exists())
 		{
 			f.createNewFile();
-			System.out.println("New file \" " + currentFile + ".txt\" has been created to the current directory");
+			System.out.println("New file \" " + currentFileOut + ".txt\" has been created to the current directory");
 		}
 		
-		FileReader inFile = new FileReader(currentFile + ".txt");
+		FileReader inFile = new FileReader(currentFileOut + ".txt");
 		
 		BufferedReader in = new BufferedReader(inFile);
 		int currentLine = 0;
@@ -363,7 +508,6 @@ public class ImageLabeller extends JFrame{
 	public static void main(String argv[]) throws IOException{
 		try {
 			//create a window and display the image
-			ImageLabeller window = new ImageLabeller();
 			window.getPolygons(window);
 			//Let the user confirm the app closing.
 			window.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
